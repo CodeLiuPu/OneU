23adf635e2e129921169786a7a8b89217a7fc03e
liupu <575866447@qq.com>
2020-04-10 12:32:18 +0800

    feat: dependencies

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+repositories {
+    mavenCentral()
+    google()
+    jcenter()
+}
+plugins {
+    `kotlin-dsl`
+    id("groovy")
+}
+dependencies {
+    gradleApi()
+    localGroovy()
+}
---
+++ buildSrc/src/main/kotlin/com/update/builds/Dependencies.kt
@@ -0,0 +1,23 @@
+object Modules {
+    const val app = ":app"
+
+}
+
+object Versions {
+    const val kotlin = "1.3.61"
+    const val kotlin_coroutines_core = "1.2.1"
+    const val kotlin_coroutines_android = "1.1.1"
+
+}
+
+object Libs {
+
+    const val androidx_appcompat = "androidx.appcompat:appcompat:1.1.0"
+    const val androidx_constraintlayoutt = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+    const val ktx_core = "androidx.core:core-ktx:1.0.2"
+    const val ktx_fragment = "androidx.fragment:fragment-ktx:1.1.0"
+    const val ktx_lifecycle_viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0"
+
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
