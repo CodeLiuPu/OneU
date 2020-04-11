6948010a52a567d2622400f2d7d1ea0687ae660d
liupu <575866447@qq.com>
2020-04-11 11:07:01 +0800

    feat: Dependencies

--- buildSrc/src/main/kotlin/com/update/builds/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/update/builds/Dependencies.kt
@@ -1,6 +1,15 @@
 object Modules {
     const val app = ":app"
+}
 
+object AppConfigs {
+    const val application_id = "com.update.oneu"
+    const val min_sdk_version = 17
+    const val target_sdk_version = 28
+    const val compile_sdk_version = 28
+    const val build_tools_version = "28.0.3"
+    const val version_code = 1
+    const val version_name = "1.0.0"
 }
 
 object Versions {
@@ -13,7 +22,7 @@ object Versions {
 object Libs {
 
     const val androidx_appcompat = "androidx.appcompat:appcompat:1.1.0"
-    const val androidx_constraintlayoutt = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val androidx_constraintlayout = "androidx.constraintlayout:constraintlayout:1.1.3"
 
     const val ktx_core = "androidx.core:core-ktx:1.0.2"
     const val ktx_fragment = "androidx.fragment:fragment-ktx:1.1.0"
